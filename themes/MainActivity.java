916956200557f95e6c5a0857ec45643e76c53166
afeefkp <93336792+afeefkp@users.noreply.github.com>
2022-10-28 15:36:29 +0530

    Create MainActivity.java

---
+++ themes/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.themes;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void click(View view)
+    {
+        Intent i=new Intent(getApplicationContext(), com.example.themes.MainActivity2.class);
+        startActivity(i);
+    }
+}
